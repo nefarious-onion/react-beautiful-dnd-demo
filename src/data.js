04ba670d456ac6048efd2defd8a944418a5b19b7
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-06-26 16:00:45 +0300

    add datafile

---
+++ src/data.js
@@ -0,0 +1,28 @@
+export const listData = {
+    tasks: {
+        'task-1': {id: 'task-1', content: 'Take the dog out'},
+        'task-2': {id: 'task-2', content: 'Paint the bedroom'},
+        'task-3': {id: 'task-3', content: 'Be a decent human being'},
+        'task-4': {id: 'task-4', content: 'Learn deno.js'},
+        'task-5': {id: 'task-5', content: 'Remember to drink water'},
+        'task-6': {id: 'task-6', content: 'fix waterpump'}
+    },
+    lists: {
+        'list-1': {
+            id: 'list-1',
+            title: 'TODO',
+            taskIds: ['task-1', 'task-2', 'task-3', 'task-4', 'task-5']
+        },
+        'list-2': {
+            id: 'list-2',
+            title: 'In progress',
+            taskIds: []
+        },
+        'list-3': {
+            id: 'list-3',
+            title: 'Done',
+            taskIds: []
+        },
+    },
+    listOrder: ['list-1', 'list-2', 'list-3'],
+}
